1be1588a406add8355c1c14608a389ef3db95513
muthu-aagnia <muthukumar.ext@aagnia.com>
2023-03-27 11:26:08 +0530

    update

---
+++ src/entity/meeting.ts
@@ -0,0 +1,56 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import { Users } from "./Users";
+import { Campaigns } from "./campaigns";
+
+export enum status {
+  APPROVED = "approved",
+  PENDING = "pending",
+}
+
+@Entity()
+export class Meeting {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne((type) => Users)
+  @JoinColumn({ name: "user_id", referencedColumnName: "id" })
+  user: Users;
+
+  @ManyToOne((type) => Campaigns, (campaign) => campaign.myDeals)
+  @JoinColumn({ name: "campaign_id", referencedColumnName: "id" })
+  campaign: Campaigns;
+
+  @Column({
+    type: "enum",
+    enum: status,
+    default: status.PENDING,
+  })
+  status: status;
+
+  @CreateDateColumn({
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP(6)",
+  })
+  createdDate: Date;
+
+  @UpdateDateColumn({
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP(6)",
+    onUpdate: "CURRENT_TIMESTAMP(6)",
+  })
+  updatedDate: Date;
+
+  @Column("boolean", { default: true })
+  is_active: boolean = true;
+
+  @Column("boolean", { default: false })
+  is_deleted: boolean = false;
+}
